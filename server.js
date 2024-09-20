fb4fb9f854b76ee837f2955d7ac8ff2b27ca5a91
Kugesz <kovacsgergo.algyo@gmail.com>
2024-09-20 10:42:14 +0200

    yepee

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 const express = require("express");
 const fs = require("fs")
 const path = require("path")
+
+const app = express();
+
+const PORT = 3000;
+app.get('/', (req, res) =>{
+    res.send(`<h1>Szerbusz!</h1>`)
+})
+
+app.listen(PORT, () =>{
+    console.log("200")
+})
